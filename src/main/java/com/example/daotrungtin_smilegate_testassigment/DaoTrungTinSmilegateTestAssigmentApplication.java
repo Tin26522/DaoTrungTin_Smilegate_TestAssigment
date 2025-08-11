905891c63c9c44c6f32faf4998c0905eff393750
tin <106314906+Tin26522@users.noreply.github.com>
2025-08-11 21:14:47 +0700

    Initial commit

---
+++ src/main/java/com/example/daotrungtin_smilegate_testassigment/DaoTrungTinSmilegateTestAssigmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.daotrungtin_smilegate_testassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoTrungTinSmilegateTestAssigmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaoTrungTinSmilegateTestAssigmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/daotrungtin_smilegate_testassigment/DaoTrungTinSmilegateTestAssigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daotrungtin_smilegate_testassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoTrungTinSmilegateTestAssigmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
